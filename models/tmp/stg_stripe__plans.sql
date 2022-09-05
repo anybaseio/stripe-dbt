c0da605002bc62546ea0257b5d754acc7f0cd2b2
Adam Rochette <adamrochette@gmail.com>
2022-09-04 22:27:26 -0400

    Switch repos back

--- models/tmp/stg_stripe__plans.sql
+++
@@ -1,9 +0,0 @@
-select
-    id as plan_id,
-    active as is_plan_active,
-    amount as plan_amount,
-    interval as plan_interval,
-    interval_count as plan_interval_count,
-    nickname as plan_nickname,
-    product as plan_product_id
-from {{ var('plans') }}
--- models/tmp/stg_stripe__refunds.sql
+++
@@ -1,8 +0,0 @@
-select
-    id as refund_id,
-    charge as charge_id,
-    amount as refund_amount,
-    reason as refund_reason,
-    status as refund_status,
-    balance_transaction as balance_transaction_id
-from {{ var('refunds')}}
